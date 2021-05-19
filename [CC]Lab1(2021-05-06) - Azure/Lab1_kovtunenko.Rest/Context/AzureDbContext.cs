f352fb7a1b9462ca2d0e2bcb6cc6e52a5ec8d6f7
Dantir <45596475+Dantir@users.noreply.github.com>
2021-05-19 15:27:56 +0200

    Add files via upload

---
+++ [CC]Lab1(2021-05-06) - Azure/Lab1_kovtunenko.Rest/Context/AzureDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Lab1_kovtunenko.Rest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1_kovtunenko.Rest.Context
+{
+    public class AzureDbContext : DbContext
+    {
+        public AzureDbContext(DbContextOptions<AzureDbContext> options) : base(options)
+        { }
+        protected AzureDbContext()
+        { }
+        public DbSet<Person> People { get; set; }
+    }
+}
